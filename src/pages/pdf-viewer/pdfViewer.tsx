2e9f33921acfad9ac29af91b1b48918fa45b37f4
An7112 <an.nguyent@gotecq.com>
2023-06-12 10:50:15 +0700

    update: ui

--- src/pages/pdf-viewer/pdfViewer.tsx
+++ src/pages/pdf-viewer/pdfViewer.tsx
@@ -2,6 +2,7 @@ import Tabs from 'component/tabs/tabs'
 import React, { useCallback } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import './pdfviewer.css'
 
 interface TabProps {
   tabKey: string;
@@ -16,24 +17,24 @@ const CustomListView: React.FC<TabProps> = (props) => {
     <iframe src={file} title="PDF Viewer" width="100%" height="600px" />
   `;
   return (
-    <div className='container-custom paginated'>
-      <h3>Bạn có thể sử dụng embed và iframe để tạo chức năng pdf viewer, hoặc có thể sử dụng thư viện ngoài.</h3>
+    <div className='container-custom pdfviewer'>
+      <h3>You can use embed and iframe to create pdf viewer functionality, or you can use an external library.</h3>
 
       <div className='source-code'>
         <h3>Embed</h3>
-        <p>Thẻ embed hỗ trợ nhúng các loại tệp đa phương tiện, bao gồm cả PDF. 
-        Nó cho phép bạn tùy chỉnh kích thước và vị trí của trình xem PDF</p>
-        <p>Ưu điểm của embed là nó đơn giản và dễ sử dụng. 
-        Tuy nhiên, nó có thể gặp một số vấn đề tương thích trên một số trình duyệt cũ hoặc hệ điều hành.</p>
+        <p>The embed tag supports embedding multimedia file types, including PDF.
+        It allows you to customize the size and position of the PDF viewer</p>
+        <p>The advantage of embed is that it is simple and easy to use.
+        However, it may have some compatibility issues on some older browsers or operating systems.</p>
         <SyntaxHighlighter language="javascript" style={materialDark}>
           {embed}
         </SyntaxHighlighter>
       </div>
       <div className='source-code'>
         <h3>Iframe</h3>
-        <p>Thẻ iframe cũng cho phép nhúng các tệp PDF vào trang web, và nó cũng hỗ trợ nhúng các trang web khác.</p>
-        <p>Ưu điểm của iframe là nó được hỗ trợ tốt trên hầu hết các trình duyệt và hệ điều hành. 
-          Tuy nhiên, một số vấn đề an ninh có thể phát sinh khi nhúng nội dung từ một nguồn bên ngoài.</p>
+        <p>The iframe tag also allows embedding PDF files into web pages, and it also supports embedding other web pages.</p>
+        <p>The advantage of iframe is that it is well supported on most browsers and operating systems.
+          However, some security issues can arise when embedding content from an external source.</p>
         <SyntaxHighlighter language="javascript" style={materialDark}>
           {iframe}
         </SyntaxHighlighter>
